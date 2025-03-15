05bc7ee721bc126fe522bdc53640cf09535c9b05
fabohax <40230@pm.me>
2025-03-15 20:18:24 -0300

    .

--- app/page.tsx
+++ app/page.tsx
@@ -1,5 +1,6 @@
 // app/page.tsx
 'use client';
+import Link from "next/link";
 import { Button } from "@/components/ui/button";
 
 export default function MainPage() {
@@ -8,7 +9,7 @@ export default function MainPage() {
     <div className="s-full h-screen bg-white text-black m-0 p-0 overflow-hidden text-center py-72" id="__next">
       <h1>LANDIN PAGE</h1>
       <br/>
-      <Button>Launch App</Button>
+      <Link href="/app"><Button>Launch App</Button></Link>
     </div>
   );
 }
